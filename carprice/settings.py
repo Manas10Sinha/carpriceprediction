44a2d1195d5acb1c0031dc5b6fd0bddb014749e5
Manas_Sinha <82819911+Manas10Sinha@users.noreply.github.com>
2024-12-15 09:04:04 +0530

    Update settings.py

--- carprice/settings.py
+++ carprice/settings.py
@@ -26,12 +26,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 #DEBUG = True
 DEBUG = os.environ.get("DEBUG" , "False").lower() == "true"
 
-#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = ["carprice105.herokuapp.com","localhost"]
 #ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 #ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
 #ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost carpriceprediction-1kya.onrender.com").split()
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost carpriceprediction-qg4b.onrender.com").split()
+#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost carpriceprediction-qg4b.onrender.com").split()
 
 
 
